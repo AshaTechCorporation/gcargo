e5364514eeed0000e6122893648d18d840cb1010
ChanuT3 <Chanuk_loveza@hotmail.com>
2025-08-27 17:13:45 +0700

    set noti

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gcargo/auth/auth_wrapper.dart';
 import 'package:gcargo/controllers/home_controller.dart';
+import 'package:gcargo/firebase_options.dart';
 import 'package:gcargo/services/cart_service.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+
+  OneSignal.initialize("5b612135-a020-4fa7-8a95-37aa830870b4");
 
   // Initialize Hive
   await Hive.initFlutter();
