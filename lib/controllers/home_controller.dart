1a9b3c77dd256d91e27df549ac2a47fccd900b8a
ChanuT3 <Chanuk_loveza@hotmail.com>
2025-10-04 13:06:37 +0700

    bank

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -68,8 +68,7 @@ class HomeController extends GetxController {
       final data = await HomeService.getItemSearch(search: query, type: selectedItemType.value == 'shopgs1' ? 'taobao' : '1688', page: 1);
 
       if (data != null && data is Map && data['item'] is Map && data['item']['items'] is Map && data['item']['items']['item'] is List) {
-        final List<Map<String, dynamic>> items =
-            (data['item']['items']['item'] as List).whereType<Map>().map((e) => Map<String, dynamic>.from(e)).toList();
+        final List<Map<String, dynamic>> items = (data['item']['items']['item'] as List).whereType<Map>().map((e) => Map<String, dynamic>.from(e)).toList();
 
         if (items.isNotEmpty) {
           searchItems.value = items;
